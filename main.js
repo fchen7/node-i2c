6873c6c9c27d6aa9a78af9e68e64921e47fa23b7
FredChen7 <fchen7@logitech.com>
2018-08-16 22:54:41 +0800

    i2c, support async write

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
 require('coffee-script');
 require('coffee-script/register');
 var i2c = require('./lib/i2c');
+i2c.prototype.scanAsync = function() {
+    return new Promise((resolve, reject) => {
+        this.scan((error) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+i2c.prototype.writeBytesAsync = function(addr, bytesToWrite) {
+    return new Promise((resolve, reject) => {
+        this.writeBytes(addr, bytesToWrite, error => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+i2c.prototype.writeAsync = function(buf) {
+    return new Promise((resolve, reject) => {
+        this.write(buf, error => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
 
 module.exports = i2c;

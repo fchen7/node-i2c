73f2aa39f3460d480ede0e913a4ad9e9265e0196
Daniel Tsai <dtsai1@logitech.com>
2018-08-20 15:52:59 +0800

    i2c, add writeByteAsync and readAsync

--- main.js
+++ main.js
@@ -12,6 +12,17 @@ i2c.prototype.scanAsync = function() {
         });
     });
 };
+i2c.prototype.writeByteAsync = function(byteToWrite) {
+    return new Promise((resolve, reject) => {
+        this.writeByte(byteToWrite, error => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
 i2c.prototype.writeBytesAsync = function(addr, bytesToWrite) {
     return new Promise((resolve, reject) => {
         this.writeBytes(addr, bytesToWrite, error => {
@@ -30,6 +41,18 @@ i2c.prototype.writeAsync = function(buf) {
                 reject(error);
             } else {
                 resolve();
+            }
+        });
+    });
+};
+
+i2c.prototype.readAsync = (nBytes) => {
+    return new Promise((resolve, reject) => {
+        this.read(nBytes, function (error, res) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(res);
             }
         });
     });
